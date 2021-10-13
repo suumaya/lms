b8ba42d42e1b16d4776ac12d505a64e3a8905f52
AbdulrahmanAlaql <74560332+AbdulrahmanAlaql@users.noreply.github.com>
2021-10-13 22:42:30 +0300

    Revert "adding  hierarchy for database connection and putting setters and"
    This reverts commit 10c51f8a1405c069824b105a82c9020edd162613.

--- LMS/src/tables/admin.java
+++ LMS/src/tables/admin.java
@@ -30,28 +30,6 @@ import javax.persistence.NamedQuery;
 @Table(name = "admin")
 public class admin implements Serializable {
 
-	private String userName;
-	private String passowrd;
-	
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getPassowrd() {
-		return passowrd;
-	}
-	public void setPassowrd(String passowrd) {
-		this.passowrd = passowrd;
-	}
-	
-	
-	
-	
-	
 }
-
-
 
 

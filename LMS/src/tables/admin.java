5677cd4360dbedf089b42cef93aafc7f6f0577c0
DeemSameer <66889295+DeemSameer@users.noreply.github.com>
2021-10-15 11:45:45 +0300

    To solve conflict

--- LMS/src/tables/admin.java
+++ LMS/src/tables/admin.java
@@ -29,7 +29,22 @@ import javax.persistence.NamedQuery;
 @Entity
 @Table(name = "admin")
 public class admin implements Serializable {
-
+	private String id;
+	private String password;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
 
 
--- LMS/src/tables/student.java
+++ LMS/src/tables/student.java
@@ -29,7 +29,57 @@ import javax.persistence.NamedQuery;
 @Entity
 @Table(name = "student")
 public class student implements Serializable {
-
+	private String userName;
+	private String password;
+	private String email; 
+	private String firstName;
+	private String lastName;
+	private String department;
+	private int level;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	
+	
 }
 
 
